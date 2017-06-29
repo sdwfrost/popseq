cd28132c9d1588fc75f131b02c6eeba1556969e6
Fei Xiang <xf3087@gmail.com>
2017-06-29 04:46:31 +0100

    update screen_select.R

--- R/screen_select.R
+++ R/screen_select.R
@@ -21,13 +21,15 @@
 
 
 
+
+
+
 #########an overall function to deal with a list of fasta files on selection#######
 screen_select=function(file_list,ref=NULL,priorPA=.001){
-  require(plyr)
   #########seq from a list of fasta files######
   seq.fas=fastaTolist(fas_list = file_list,ref=ref)
   pstar=seq.fas$pstar
-  freq.mat=seq.fas$freq.mat
+  freq.mat=seq.fas$freq.mat.ordered
   cons=seq.fas$cons
   ######calculate the ppas########
 site.PPA_SI=0
@@ -36,6 +38,11 @@ site.PPA_SI=0
     result=ppa.gen(freqmatrix =  M,pstar=pstar,priorPA=priorPA)
     return(result$PPA_SI)
     })
+  names(site.PPA_SI)=NULL
+  site.and.PPA_SI=cbind(site=1:dim(freq.mat)[2],PPA_SI=site.PPA_SI)
+   output=list(pstar=pstar,basedist=seq.fas$freq.mat,
+               cons=cons,site.PPA_SI=t(site.and.PPA_SI),
+               site.interest=site.and.PPA_SI[site.and.PPA_SI[,2]>.5,])
+   return(output)
 
-  site.PPA_SI
 }
